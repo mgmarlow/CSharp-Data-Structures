2a9ece4b518a557335d2adb4594ce9c52f31f01c
Graham Marlow <mgmarlow@gmail.com>
2016-05-22 11:55:34 -0700

    Add DoublyLinkedList

---
+++ DataStructures/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.DoublyLinkedList
+{
+    public class DoublyLinkedList<T> : ICollection<T>
+    {
+        public DoublyLinkedListNode<T> Head { get; private set; }
+        public DoublyLinkedListNode<T> Tail { get; private set; }
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Create a node from a value and add it to the front of a list.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddFirst(T value)
+        {
+            AddFirst(new DoublyLinkedListNode<T>(value));
+        }
+
+        /// <summary>
+        /// Add a node to the front of the list.
+        /// </summary>
+        /// <param name="node"></param>
+        public void AddFirst(DoublyLinkedListNode<T> node)
+        {
+            DoublyLinkedListNode<T> temp = Head;
+            Head = node;
+            Head.Next = temp;
+            Count++;
+
+            if (Count == 1)
+            {
+                Tail = Head;
+            }
+            else
+            {
+                temp.Previous = Head;
+            }
+        }
+
+        /// <summary>
+        /// Create a node from a value and add it to the end of a list.
+        /// </summary>
+        /// <param name="value"></param>
+        public void AddLast(T value)
+        {
+            AddLast(new DoublyLinkedListNode<T>(value));
+        }
+
+        /// <summary>
+        /// Add a node to the end of the list.
+        /// </summary>
+        /// <param name="node"></param>
+        public void AddLast(DoublyLinkedListNode<T> node)
+        {
+            if (Count == 0)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.Next = node;
+                node.Previous = Tail;
+            }
+            Tail = node;
+            Count++;
+        }
+
+        /// <summary>
+        /// Remove the first element of the list.
+        /// </summary>
+        public void RemoveFirst()
+        {
+            if (Count != 0)
+            {
+                Head = Head.Next;
+                Count--;
+
+                if (Count == 0)
+                {
+                    Tail = null;
+                }
+                else
+                {
+                    Head.Previous = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Remove the last element of the list.
+        /// </summary>
+        public void RemoveLast()
+        {
+            if (Count != 0)
+            {
+                if (Count == 1)
+                {
+                    Head = null;
+                    Tail = null;
+                }
+                else
+                {
+                    Tail.Previous.Next = null;
+                    Tail = Tail.Previous;
+                }
+
+                Count--;
+            }
+        }
+
+        // ICollection
+        /// <summary>
+        /// Add a new element to the front of the list.
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            AddFirst(item);
+        }
+
+        /// <summary>
+        /// Check if the list contains the given element.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Contains(T item)
+        {
+            DoublyLinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Copy the values of the linked list into an array.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            DoublyLinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        /// <summary>
+        /// Return the true if the list is readonly.
+        /// </summary>
+        public bool IsReadOnly => false;
+
+        /// <summary>
+        /// Remove the first occurance of an element from the list.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Remove(T item)
+        {
+            DoublyLinkedListNode<T> previous = null;
+            DoublyLinkedListNode<T> current = Head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    // A node in the middle or end
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                        {
+                            Tail = previous;
+                        }
+                        else
+                        {
+                            current.Next.Previous = previous;
+                        }
+
+                        Count--;
+                    }
+                    else
+                    {
+                        RemoveFirst();
+                    }
+
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Enumerates over the linked list values from Head to Tail.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyLinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        /// <summary>
+        /// Enumerates over the linked list values from Head to Tail.
+        /// </summary>
+        /// <returns></returns>
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return ((System.Collections.Generic.IEnumerable<T>)this).GetEnumerator();
+        }
+
+        /// <summary>
+        /// Removes all the nodes from the list.
+        /// </summary>
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+    }
+}
+}
---
+++ DataStructures/DoublyLinkedList/DoublyLinkedListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructures.DoublyLinkedList
+{
+    public class DoublyLinkedListNode<T>
+    {
+        public T Value { get; set; }
+        public DoublyLinkedListNode<T> Next { get; set; }
+        public DoublyLinkedListNode<T> Previous { get; set; }
+
+        public DoublyLinkedListNode (T value)
+        {
+            Value = value;
+        } 
+    }
+}
